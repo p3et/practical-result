f16daa245107bc3b1a0a765587feefab740a9b14
André Petermann <andre.petermann@jambit.com>
2021-09-16 10:16:49 +0200

    Typescript: Fix type of flatMap constant

--- typescript/src/flatMap.ts
+++ typescript/src/flatMap.ts
@@ -1,9 +1,10 @@
 import {failure, Result, success} from "./result";
 import {listService} from "./listService";
 
-const flatMapped = success(4)
-    .flatMap(list => listService(list))
-    .flatMap(list => list.length == 0 ? failure("Empty list")
-                                      : success(list));
+const flatMapped: Result<string, number[]> =
+    success<string, number>(4)
+        .flatMap(list => listService(list))
+        .flatMap(list => list.length == 0 ? failure("Empty list")
+                                          : success(list));
 
 console.log(flatMapped)
